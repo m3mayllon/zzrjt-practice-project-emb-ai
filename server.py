a819b8f92868de6923142da73edc5fdda9c3ef89
mayllon <87451748+m3mayllon@users.noreply.github.com>
2024-03-19 08:55:46 -0700

    chore: linting

--- server.py
+++ server.py
@@ -5,9 +5,10 @@
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
 
-#Initiate the flask app : TODO
+# Initiate the flask app : TODO
 
-@app.route("/sentimentAnalyzer")
+
+@app.route('/sentimentAnalyzer')
 def sent_analyzer():
     ''' This code receives the text from the HTML interface and 
         runs sentiment analysis over it using sentiment_analysis()
@@ -16,13 +17,15 @@ def sent_analyzer():
     '''
     # TODO
 
-@app.route("/")
+
+@app.route('/')
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    # TODO
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''  # TODO
