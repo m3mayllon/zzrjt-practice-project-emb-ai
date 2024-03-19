2af6fdbe34f3930a91ae7a72d7a288b418a3193a
mayllon <87451748+m3mayllon@users.noreply.github.com>
2024-03-19 09:45:07 -0700

    feat: create app routers

--- server.py
+++ server.py
@@ -1,31 +1,36 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from nlp.sentiment import sentiment_analyzer
 
-# Initiate the flask app : TODO
-
-
-@app.route('/sentimentAnalyzer')
-def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
-    # TODO
+app = Flask('Sentiment Analyzer')
 
 
 @app.route('/')
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
     '''
-    # TODO
+    This function initiates the rendering of the main application
+    page over the Flask channel
+    '''
+    return render_template('index.html')
+
+
+@app.route('/sentimentAnalyzer')
+def sent_analyzer():
+    '''
+    This code receives the text from the HTML interface and 
+    runs sentiment analysis over it using sentiment_analysis()
+    function. The output returned shows the label and its confidence 
+    score for the provided text.
+    '''
+
+    text = request.args.get('textToAnalyze')
+    data = sentiment_analyzer(text)
+
+    label = data['label']
+    score = data['score']
+
+    return f'The given text has been identified as {label} with a score of {score}.', 200
 
 
 if __name__ == '__main__':
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''  # TODO
+
+    app.run(host='0.0.0.0', port=8080, debug=True)
